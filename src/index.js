d1eb493299196ab37a4fac4e998507df62da1ec7
tarasovcad <tarasovcad@gmail.com>
2024-10-27 15:05:20 -0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let decodedMessage = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        const chunk = expr.slice(i, i + 10);
+
+        if (chunk === "**********") {
+            decodedMessage += " ";
+            continue;
+        }
+        let morseCode = '';
+        for (let j = 0; j < chunk.length; j += 2) {
+            const pair = chunk.slice(j, j + 2);
+            if (pair === "10") {
+                morseCode += '.';
+            } else if (pair === "11") {
+                morseCode += '-';
+            }
+        }
+
+        decodedMessage += MORSE_TABLE[morseCode] || ''; 
+    }
+
+    return decodedMessage;
 }
 
 module.exports = {
